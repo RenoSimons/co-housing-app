5ccea2a18fe12e01af9514a901aae00ce9b8a9b4
reno simons <simons.reno@skynet.be>
2021-06-08 23:12:48 +0200

    //

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,7 +3,6 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
-
 class Kernel extends HttpKernel
 {
     /**
@@ -37,7 +36,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\HttpsProtocol::class,
+            \App\Http\Middleware\HttpsProtocolMiddleware::class,
         ],
 
         'api' => [
